477e9598b766180e9bd94260997b162057b87885
astanley-work <dmstn@amazon.com>
2024-12-19 15:56:09 -0800

    Adding input validation to ScalingConfig resources_per_worker
    Signed-off-by: astanley-work <dmstn@amazon.com>

--- python/ray/air/config.py
+++ python/ray/air/config.py
@@ -177,11 +177,17 @@ class ScalingConfig:
                     "`resources_per_worker."
                 )
             
+            # Validate inputs for resources_per_worker
             accepted_resource_arguments: List[str] = ["GPU", "CPU", "memory", "custom"]
-            if len([key for key in self.resources_per_worker if key not in accepted_resource_arguments]) >= 1:
+            invalid_resources = []
+            for key in self.resources_per_worker.keys():
+                if key not in accepted_resource_arguments:
+                    invalid_resources.append(key)
+            if len(invalid_resources) >= 1:
                 raise ValueError(
                     f"The resources_per_worker argument expects keys from the list"
-                    f"in {accepted_resource_arguments}. You have provided invalid resource names {[key for key in self.resources_per_worker.keys() if key not in accepted_resource_arguments]}"
+                    f"in {accepted_resource_arguments}. You have provided invalid"
+                    f"resource names {invalid_resources}"
                 )
 
     def __repr__(self):
